ed7631f73812e0ca51036782e24f18ec9de1bf52
quasar0204 <quasar13245@gmail.com>
2021-06-23 21:15:06 +0900

    댓글 유저 인증 미들웨어 추가

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,8 +1,13 @@
-const { Post } = require('../database/models');
+const { Post, Comment } = require('../database/models');
 
 exports.PostAuth = async (ctx, next) => {
-    const post = await Post.findOne({ where: {id: ctx.params.id} });
-    console.log('ctx.state.user.id', ctx.state.user.id, 'post.userId', post.userId);
+    let post = await Post.findOne({ where: {id: ctx.params.id} });
+
+    if (!post) {
+        console.log('게시글이 없습니다.')
+        return ctx.redirect('/post');
+    }
+
     if (ctx.state.user.id == post.userId) {
         console.log('작성자 입니다.');
         await next();
@@ -12,3 +17,22 @@ exports.PostAuth = async (ctx, next) => {
         return ctx.redirect('/post');
     }
 };
+
+exports.CommentAuth = async (ctx, next) => {
+    console.log('params', ctx.params);
+    let comment = await Comment.findOne({ where: {id: ctx.params.commentId} });
+    
+    if (!comment) {
+        console.log('댓글이 없습니다.')
+        return ctx.redirect('/post');
+    }
+    console.log('ctx.state', ctx.state);
+    if (ctx.state.user.id == comment.userId) {
+        console.log('작성자 입니다.');
+        await next();
+    }
+    else {
+        console.log('권한이 없습니다.');
+        return ctx.redirect('/post');
+    }
+}
